3d0d3f9cb06189f1533e1ed2f7c4b6dc6a4d496f
Angry Nimfa <angrynimfa@Angrys-iMac.local>
2019-05-31 18:27:30 -0700

    Working on MD5

--- md5.c
+++ md5.c
@@ -26,7 +26,7 @@ void			md5_init(t_fmd5 *fmd, char *str)
 	fmd->bitlen = fmd->len * 8;
 }
 
-unsigned	*md5_update(t_fmd5 *fmd, char *str, unsigned *x)
+unsigned	char *md5_update(t_fmd5 *fmd, char *str, unsigned char *x)
 {
 	int				rem;
 	int				size;
@@ -41,21 +41,6 @@ unsigned	*md5_update(t_fmd5 *fmd, char *str, unsigned *x)
 	//size = fmd->len + 1;
 	ft_memcpy((char *)x, str, fmd->len);
 	((char *)x)[fmd->len] = 0x80;
-	i = 0;
-	printf("\n");
-	while (i < 16)
-	{
-		printf("%u", x[i]);
-		i++;
-	}
-	printf("\n");
-	//ft_strcat((char *)x, ft_memset(&x[fmd->len + 1], 0, size));
-	// i = 0;
-	// while (i < 8)
-	// {
-	// 	x[size - 8 + i] = fmd->bitlen >> i * 8;
-	// 	i++;
-	// }
 	// i = 0;
 	// printf("\n");
 	// while (i < 16)
@@ -64,7 +49,13 @@ unsigned	*md5_update(t_fmd5 *fmd, char *str, unsigned *x)
 	// 	i++;
 	// }
 	// printf("\n");
-	// printf("\n%s\n", (char *)x);
+	ft_strcat((char *)x, ft_memset(&x[fmd->len + 1], 0, size));
+	i = 0;
+	while (i < 8)
+	{
+		x[size - 8 + i] = (unsigned char)fmd->bitlen >> i * 8;
+		i++;
+	}
 	return (x);
 }
 
@@ -103,28 +94,36 @@ void			put_hash(unsigned hash[], int size)
 
 void	ft_md5(t_fmd5 *fmd, t_flg *flg, t_alp *al, char *arg)
 {
-	unsigned *x;
-	unsigned w[16];
+	unsigned char *x;
+	//unsigned w[16];
 	int i;
-	int j;
+	//int j;
 
 	md5_init(fmd, arg);
-	x = ft_memalloc(sizeof(unsigned int) * 64);
+	x = ft_memalloc(sizeof(unsigned char) * 64);
 	md5_update(fmd, arg, x);
 	i = 0;
-	j = 0;
+	printf("\n");
 	while (i < 16)
 	{
-		w[i] = (x[j]) + (x[j + 1] << 8) + (x[j + 2] << 16) + (x[j + 3] << 24);
-		printf("%u", w[i]);
-		j = j + 4;
+		printf("%u", x[i]);
 		i++;
 	}
+	printf("\n");
+	// i = 0;
+	// j = 0;
+	// while (i < 16)
+	// {
+	// 	w[i] = (unsigned)((x[j]) + (x[j + 1] << 8) + (x[j + 2] << 16) + (x[j + 3] << 24));
+	// 	printf("%u", w[i]);
+	// 	j = j + 4;
+	// 	i++;
+	// }
 
 	// for (i=0,j=0; i < 16; ++i, j += 4) 
     //   m[i] = (data[j]) + (data[j+1] << 8) + (data[j+2] << 16) + (data[j+3] << 24); 
 	//x[15] = 0;
-	stage_one(fmd, al, w);
+	stage_one(fmd, al, (unsigned *)x);
 	if (flg->q == 0 && flg->r == 0)
 	{
 		ft_printf("MD5 (\"%s\") = ", arg);
